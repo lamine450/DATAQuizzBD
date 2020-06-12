1a7f5c333d099a195e1d4537cd9202f6d12328ae
lamine450 <62139787+lamine450@users.noreply.github.com>
2020-06-12 04:36:51 +0000

    Add files via upload

---
+++ connexion.php
@@ -0,0 +1,12 @@
+<header class="bgsecondary">
+         <h1 class="text-center m-0">j'ai le plaisir de jouer</h1>
+</header>
+      <div class="container" class="d-flex justify-content-center align-item-center">
+
+            <div  class="connexion-form-header">
+                <h3 class="bgprimary h5 p-3 m-0">connexion</h3>
+
+            </div>
+
+
+      </div>
