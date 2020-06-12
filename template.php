b2a5ee2d464b9dd0ec03b2693d5750fe4a4e3c60
lamine450 <62139787+lamine450@users.noreply.github.com>
2020-06-12 04:35:53 +0000

    Add files via upload

---
+++ template.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>connexion</title>
+    <link rel="stylesheet" href="./public/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./public/css/style.css">
+</head>
+<body>
+        <div classe="container">
+        
+                <?php
+                
+                require_once("./pages/authentique/connexion.php");
+                
+                ?>
+        
+        </div>
+        <script src="public/js/jquery.js"></script>
+
+        <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+        
+
+</body>
+</html>
